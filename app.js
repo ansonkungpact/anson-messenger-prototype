18be636824693990fc81c4cc37ec830209befc86
Anson Kung <anson_kung@cathaypacific.com>
2017-03-13 13:30:38 +0800

    udpate senddata

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const request = require('request');
 
 const app = express();
 
+var token = "EAACATjSv7U4BAKNe30LjYCBhaD9EQ51aixFve4tOwLfy3KWZBmunynCXyhFIGxmzKTcYq7AokY5YZApVp8d4kFcGLHz72KwZCLJYZCX1uq6Jf3frf6270yU5n9VdXe8jCUydK0OvcZA00VWSDOSMNTWJT07TyidZAbv24QxoCKGQZDZD";
+
 app.set('port', (process.env.PORT || 5000));
 
 // Allows us to process the data
@@ -18,13 +20,44 @@ app.get('/', function(req, res) {
 })
 
 // Facebook
-app.get('/webhook', function(req, res) {
+app.get('/webhook/', function(req, res) {
     if (req.query['hub.verify_token'] === "ansontesting") {
         res.send(req.query['hub.challenge'])
     }
     res.send("Wrong token")
 });
 
+app.post('/webhook/', function(req, res) {
+    let messaging_events = req.body.entry[0].messaging_events
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = messaging_event[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendText(sender, "text echo" + text.substring(0, 100))
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+    let messageData = {text: text}
+    request({
+        url: "http://graph.facebook.com/v2.8/me/messages",
+        qs : {access_token, token},
+        method: "POST",
+        json: {
+            receipt: {id: sender},
+            message: messageData
+        }, function(error, response, body) {
+            if (error) {
+                console.log("sending error")
+            } else if (response.body.error) {
+                console.log("response body error")
+            }
+        }
+    })
+}
 app.listen(app.get('port'), function() {
     console.log("running: port")
 })
