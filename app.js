1c00701131b1a02e44491c4a190ae1ae246ba099
Anson Kung <anson_kung@cathaypacific.com>
2017-03-13 09:35:11 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const 
+  bodyParser = require('body-parser'),
+  // config = require('config'),
+  // crypto = require('crypto'),
+  express = require('express'),
+  https = require('https'),  
+  request = require('request');
+
+var app = express();
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
+// app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid 
+// certificate authority.
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = app;
