82891c671109281de9444ec3aac4284aa3df8114
Anson Kung <anson_kung@cathaypacific.com>
2017-03-13 13:35:30 +0800

    debug

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const request = require('request');
 
 const app = express();
 
-var token = "EAAaQmdrMnZBoBABYGKQFBzhK4Jdk5C0Luob4QJrvODUyDIuysn8D82WudaX9hXsdZCyNiFEVOptk6OHErOoKmrSezzf04C4IhUlpZANKYlR4yReb23UyxLriq9D2YsQyFYRlf8QPPYhPtw5SBMnakTtqsYwkxZBkxYbXZAFwxFQZDZD";
+let token = "EAAaQmdrMnZBoBABYGKQFBzhK4Jdk5C0Luob4QJrvODUyDIuysn8D82WudaX9hXsdZCyNiFEVOptk6OHErOoKmrSezzf04C4IhUlpZANKYlR4yReb23UyxLriq9D2YsQyFYRlf8QPPYhPtw5SBMnakTtqsYwkxZBkxYbXZAFwxFQZDZD";
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -28,7 +28,7 @@ app.get('/webhook/', function(req, res) {
 });
 
 app.post('/webhook/', function(req, res) {
-    let messaging_events = req.body.entry[0].messaging_events
+    let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
         let event = messaging_event[i]
         let sender = event.sender.id
@@ -44,10 +44,10 @@ function sendText(sender, text) {
     let messageData = {text: text}
     request({
         url: "http://graph.facebook.com/v2.8/me/messages",
-        qs : {access_token, token},
+        qs : {access_token: token},
         method: "POST",
         json: {
-            receipt: {id: sender},
+            recipient: {id: sender},
             message: messageData
         }, function(error, response, body) {
             if (error) {
