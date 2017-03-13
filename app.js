f81cd5d697ce34a3867e0ddf0d9761e4eadf1275
Anson Kung <anson_kung@cathaypacific.com>
2017-03-13 15:24:46 +0800

    add generic text

--- app.js
+++ app.js
@@ -102,6 +102,50 @@ function sendTextMessage(recipientId, messageText) {
   callSendAPI(messageData);
 }
 
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
@@ -124,24 +168,6 @@ function callSendAPI(messageData) {
   });  
 }
 
-function sendText(sender, text) {
-    let messageData = {text: text}
-    request({
-        url: "http://graph.facebook.com/v2.6/me/messages",
-        qs : {access_token: token},
-        method: "POST",
-        json: {
-            recipient: {id: sender},
-            message: messageData
-        }, function(error, response, body) {
-            if (error) {
-                console.log("sending error")
-            } else if (response.body.error) {
-                console.log("response body error")
-            }
-        }
-    })
-}
 app.listen(app.get('port'), function() {
     console.log("running: port")
 })
