c1594bb3e7c363710883d62fa25af33a8f352e77
User <user@phoebe.local>
2017-03-14 14:05:51 +0800

    shop list

--- app.js
+++ app.js
@@ -88,6 +88,10 @@ function receivedMessage(event) {
         sendGenericMessage(senderID);
         break;
 
+      case 'shop':
+        sendShopMessage(senderID);
+        break;
+
       default:
         sendTextMessage(senderID, messageText);
     }
@@ -158,6 +162,53 @@ function sendTestingMessage(recipientId) {
 }
 
 function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type:"phone_number",
+              title:"Call Representative",
+              payload:"+15105551234"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
+}
+
+function sendShopMessage(recipientId) {
   var messageData = {
     recipient: {
       id: recipientId
