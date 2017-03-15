6b4e97716fc8e887dd86400a6e1e637fbcfa1d8e
User <user@phoebe.local>
2017-03-15 16:22:23 +0800

    sendLocationMessage

--- app.js
+++ app.js
@@ -557,8 +557,8 @@ function sendLocationMessage(recipientId) {
                 "elements": {
                     "element": {
                         "title": "Your location",
-                        "image_url": "https:\/\/maps.googleapis.com\/maps\/api\/staticmap?size=764x400&center="+lat+","+long+"&zoom=25&markers="+lat+","+long,
-                        "item_url": "http:\/\/maps.apple.com\/maps?q="+lat+","+long+"&z=16"
+                        "image_url": "https://maps.googleapis.com/maps/api/staticmap?size=764x400&center="+lat+","+long+"&zoom=25&markers="+lat+","+long,
+                        "item_url": "http://maps.apple.com/maps?q="+lat+","+long+"&z=16"
                     }
                 }
             }
