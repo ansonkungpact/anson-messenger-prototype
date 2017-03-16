81105bd88efd4bee04f40627b2f5c3c8814a38cc
Anson Kung <anson_kung@cathaypacific.com>
2017-03-16 14:45:21 +0800

    log

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ function receivedMessage(event) {
     }
   } else if (messageAttachments) {
     console.log('------------------------');
-    console.log(messageAttachments);
+    console.log(message);
     console.log('------------------------');
     sendTextMessage(senderID, "Message with attachment received");
     // console.log(message.attachments.delivery);
