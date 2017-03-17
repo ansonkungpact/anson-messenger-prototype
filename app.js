c21c12cf4259311ac42d115c8723577ffcead1ac
Anson Kung <anson_kung@cathaypacific.com>
2017-03-17 11:43:58 +0800

    log

--- app.js
+++ app.js
@@ -65,6 +65,8 @@ app.post('/webhook', function (req, res) {
 });
 
 function receivedMessage(event) {
+  console.log(event.sender);
+  console.log(event.recipient);
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
