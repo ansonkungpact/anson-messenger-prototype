82870aa1ded1f3c78710be65b829a088492263c5
Anson Kung <anson_kung@cathaypacific.com>
2017-03-20 15:00:43 +0800

    test typing

--- app.js
+++ app.js
@@ -232,7 +232,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  // sendTextMessage(senderID, "Postback called");
 }
 
 function sendTextMessage(recipientId, messageText) {
@@ -275,7 +275,7 @@ function sendTestingMessage(recipientId) {
           ]
         }
       }
-    },
+    }
   };  
 
   callSendAPI(messageData);
